0dc7872502a39ebe3ada5a14bad9dd639b9e3952
chinhpv <chinhpv@icheck.vn>
2020-01-17 09:50:11 +0700

    even bus

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -6,8 +6,11 @@
             <input v-else class="todo-item-edit" type="text" v-model="title" 
             @blur="doneEdit" @keyup.enter="doneEdit" @keyup.esc="cancelEdit" v-focus>
         </div>
-        <div class="remove-item" @click="removeTodo(index)">
-            &times;
+        <div>
+            <button @click="pluralize">Plural</button>
+            <span class="remove-item" @click="removeTodo(index)">
+                &times;
+            </span>
         </div>
     </div>
 </template>
@@ -49,6 +52,14 @@ export default {
         }
     },
 
+    created(){
+        eventBus.$on('pluralize', this.handlePluralize)
+    },
+
+    beforeDestroy(){
+        eventBus.$off('pluralize', this.handlePluralize);
+    },
+
     watch:{
         checkAll(){
             // if(this.checkAll){
@@ -62,7 +73,7 @@ export default {
 
     methods: {
         removeTodo(index){
-            this.$emit('removedTodo', index);
+            eventBus.$emit('removedTodo', index);
         },
 
         editTodo(){
@@ -75,7 +86,7 @@ export default {
                 this.title = this.beforeEditCache;
             }
             this.editing = false;
-            this.$emit('finishedEdit', {
+            eventBus.$emit('finishedEdit', {
                 'index': this.index,
                 'todo': {
                     'id': this.id,
@@ -89,6 +100,23 @@ export default {
         cancelEdit(){
             this.title = this.beforeEditCache;
             this.editing = false;
+        },
+
+        pluralize(){
+            eventBus.$emit('pluralize')
+        },
+
+        handlePluralize(){
+            this.title = this.title + 's'
+            eventBus.$emit('finishedEdit', {
+                'index': this.index,
+                'todo': {
+                    'id': this.id,
+                    'title': this.title,
+                    'completed': this.completed,
+                    'editing': this.editing
+                }
+            })
         }
     }
 }
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,25 +4,23 @@
             @keyup.enter="addTodo">
 
         <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
-            <todo-item v-for="(todo, index) in todosFiltered" :key="todo.id" :todo="todo" :index="index" @removedTodo="removeTodo" @finishedEdit="finishedEdit" :checkAll="!anyRemaining">
+            <todo-item v-for="(todo, index) in todosFiltered" :key="todo.id" :todo="todo" :index="index" :checkAll="!anyRemaining">
             </todo-item >
         </transition-group>
 
         <div class="extra-container">
-            <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos">Check All</label></div>
-            <div>{{remaining}} items left</div>
+            <todo-check-all :anyRemaining="anyRemaining"></todo-check-all>
+            <!-- <div><label><input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos">Check All</label></div> -->
+            <!-- <div>{{remaining}} items left</div> -->
+            <todo-items-remaining :remaining="remaining"></todo-items-remaining>
         </div>
 
         <div class="extra-container">
-            <div>
-                <button :class="{active: filter === 'all'}" @click="filter = 'all'">All</button>
-                <button :class="{active: filter === 'active'}" @click="filter = 'active'">Active</button>
-                <button :class="{active: filter === 'completed'}" @click="filter = 'completed'">Completed</button>
-            </div>
+            <todo-filtered></todo-filtered>
 
             <div>
                 <transition name="fade">
-                    <button v-if="showClearCompletedButton" @click="clearCompleted">Clear Completed</button>
+                    <todo-clear-completed :showClearCompletedButton="showClearCompletedButton"></todo-clear-completed>
                 </transition>
             </div>
         </div>
@@ -31,11 +29,19 @@
 
 <script>
 import TodoItem from './TodoItem'
+import TodoItemsRemaining from './TodoItemsRemaining'
+import TodoCheckAll from './TodoCheckAll'
+import TodoFiltered from './TodoFiltered'
+import TodoClearCompleted from './TodoClearCompleted'
 
 export default {
     name: 'todo-list',
     components: {
         TodoItem,
+        TodoItemsRemaining,
+        TodoCheckAll,
+        TodoFiltered,
+        TodoClearCompleted
     },
     data () {
         return {
@@ -59,13 +65,26 @@ export default {
             ]
         }
     },
+
+    created(){
+        eventBus.$on('removedTodo', (index) => this.removeTodo(index))
+        eventBus.$on('finishedEdit', (data) => this.finishedEdit(data))
+        eventBus.$on('checkAllChanged', (checked) => this.checkAllTodos(checked))
+        eventBus.$on('filterChanged', (filter) => this.filter = filter)
+        eventBus.$on('clearCompletedTodos', () => this.clearCompleted())
+    },
+    
+    beforeDestroy(){
+        
+    },
+
     computed: {
         remaining(){
             let a = this.todos.filter(todo => !todo.completed);
             return a.length;
         },
         anyRemaining(){
-            return this.remaining
+            return this.remaining !=0
         },
         todosFiltered(){
             if(this.filter == 'all'){
@@ -108,6 +127,7 @@ export default {
         },
 
         finishedEdit(data){
+            console.log(data);
             this.todos.splice(data.index, 1, data.todo);
         }
     }
