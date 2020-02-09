e6077f56704b1bbb965b9475bed78e27ccabe850
chinhpv <chinhpv@icheck.vn>
2020-02-09 18:09:15 +0700

    vue route

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="container">
+  <div id="app2" class="container">
     <img src="./assets/logo.png" class="logo">
     <todo-list/>
   </div>
@@ -27,15 +27,14 @@ export default {
 }
 
 
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
   margin-top: 60px;
   font-size: 24px;
-}
+} */
 
 .logo{
   display: block;
---
+++ src/components/marketing/LandingPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex-center">
+        Landing page content here
+    </div>
+</template>
